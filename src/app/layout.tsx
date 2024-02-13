918bff18a14955a0e1c52ea5aed75afc0571abd1
Paralitik <sergeylobodanov@gmail.com>
2024-02-13 18:13:52 +0300

    added Modals and Delete Chat API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='bg-purple-dark h-full overflow-hidden relative'>
+        <div className='bg-purple-dark h-full overflow-hidden fixed w-full'>
           <AuthContext>
             <ToasterContext />
             {children}
